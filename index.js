a3227f8f55a5b20f08c2307cf92a2e67faf3eccd
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-01-31 13:48:30 -0600

    minified stylesheet, dropped +1,-1 starting to use parcel to pack it up

---
+++ index.js
@@ -0,0 +1,18 @@
+import emojis from './emojis';
+import './demo-style.css';
+
+const list = document.querySelector('ul');
+
+const filterDisplayedEmojis = ({target}) => {
+    displayEmojis(emojis.filter(e => e.includes(target.value.toLowerCase())));
+};
+
+const displayEmojis = selected => {
+    list.innerHTML = selected.reduce((fragment, code) => {
+        return `${fragment}<li title="${code}"><i class="${code}"></i><p>${code}</p></li>`;
+    }, '');
+};
+
+displayEmojis(emojis);
+document.querySelector('#filter')
+    .addEventListener('input', filterDisplayedEmojis);
