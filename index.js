416b05dd3dadcd9c019c98d2720baf57791cd2ac
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-02-01 16:49:40 -0600

    trying to improve filter performance

--- index.js
+++ index.js
@@ -2,18 +2,42 @@ import emojis from './emojis';
 import './demo-style.css';
 import './emojis.css';
 
-const list = document.querySelector('ul');
+const main = document.querySelector('main');
+
+const hideIcon = li => li.className = 'hidden';
+
+const showIcon = li => li.className = '';
 
 const filterDisplayedEmojis = ({target}) => {
-    displayEmojis(emojis.filter(e => e.includes(target.value.toLowerCase())));
+    const query = target.value.toLowerCase();
+    const emojis = Array.from(main.querySelectorAll('li'));
+    const callbacks = emojis
+        .map(li => !query || li.textContent.includes(query) ? () => showIcon(li) : () => hideIcon(li))
+        .forEach(cb => cb());
 };
 
-const displayEmojis = selected => {
-    list.innerHTML = selected.reduce((fragment, code) => {
-        return `${fragment}<li title="${code}"><i class="${code}"></i><p>${code}</p></li>`;
-    }, '');
+const createElem = (tag, attrs, text) => {
+    const elem = document.createElement(tag);
+    if (attrs) {
+        Object.entries(attrs)
+            .forEach(([name, value]) => elem.setAttribute(name, value));
+    }
+    if (text) {
+        elem.textContent = text;
+    }
+    return elem;
 };
 
-displayEmojis(emojis);
 document.querySelector('#filter')
     .addEventListener('input', filterDisplayedEmojis);
+
+const icons = emojis.reduce((fragment, code) => {
+    const li = createElem('li', {title: code});
+    const icon = createElem('i', {class: code});
+    const p = createElem('p', null, code);
+    li.appendChild(icon);
+    li.appendChild(p);
+    fragment.appendChild(li);
+    return fragment;
+}, createElem('ul'));
+main.appendChild(icons);
